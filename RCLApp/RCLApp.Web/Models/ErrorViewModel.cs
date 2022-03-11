9f1c5c62245be2eae92db8dfbeaf1ced1278276d
Drew Brasher <abrasher79@gmail.com>
2022-03-11 12:05:00 -0600

    Added RCLApp with MVC project

---
+++ RCLApp/RCLApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RCLApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
