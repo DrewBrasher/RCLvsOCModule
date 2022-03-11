ba56dbe1324acb66507d22a72c45bb1d4eb4b5a0
Drew Brasher <abrasher79@gmail.com>
2022-03-11 14:24:14 -0600

    Added an Orchard Core Module project and referenced it from the web app.

---
+++ OCApp/Feature1/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "Feature1",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "Feature1",
+    Category = "Feature1"
+)]
